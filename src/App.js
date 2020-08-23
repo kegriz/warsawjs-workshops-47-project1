7691daa8abcd7fca061c9bf75a94c351297de571
Rafał Rudko <rafal.rudko@rb.com>
2020-08-23 11:07:46 +0200

    add button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+
+function Button(props) { // {color, text} - destrukturyzacja
+  console.log(props);
+  console.log(props.color);
+  return <button onClick={clickHandler}>Click me, {props.text}</button>;
+}
+
+function clickHandler(event) { // synthetic event
+  // event.preventDefault();
+  console.log("click", event);
+}
+
+Button.defaultProps = {
+  text: "Hello!!!",
+};
 
 function App() {
   return (
     <div className="App">
-
+      <Button color="blue" text="Hello" />
     </div>
   );
 }
